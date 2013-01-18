0df5ecabdc2ee83fedf0c0f9996e06eb851408e6
Samuel Lucidi <slucidi@newstex.com>
2013-01-18 02:58:28 -0500

    Created basic liveconnect auth adapter, added setup script.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+	name = "liveconnect",
+	version = "0.1.2",
+	packages = find_packages(),
+	install_requires = ['requests'],
+	package_data = {
+		'' : ['*.md']
+	},
+	author = "Samuel Lucidi",
+	author_email = "mansam@csh.rit.edu",
+	description = "LiveConnect OAuth adapter for Python",
+	license = "MIT",
+	keywords = "liveconnect skydrive",
+	url = "http://github.com/mansam/liveconnect"
+)
